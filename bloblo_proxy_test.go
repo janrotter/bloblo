4b5f974ac71c006c9211296629d720c84591c67a
Jan Rotter <12612258+janrotter@users.noreply.github.com>
2022-05-02 21:58:34 +0200

    test: Add a test for the blob upload

--- bloblo_proxy_test.go
+++ bloblo_proxy_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"net/http/httputil"
@@ -28,40 +29,113 @@ func (cache *testObjectStorageCache) getPresignedUrl(blobDigest string) (string,
 
 func (cache *testObjectStorageCache) isBlobInCache(blobDigest string) (bool, error) {
 	cache.checkedForBlob = true
-	return false, nil
+
+	return cache.uploadedBlob, nil
 }
 
 func (cache *testObjectStorageCache) uploadBlob(blobDigest string, body io.Reader) error {
 	cache.uploadedBlob = true
+	io.ReadAll(body)
 	return nil
 }
 
-func TestFallbackForNotCacheablePath(t *testing.T) {
+type testBackend struct {
+	server *httptest.Server
+
+	defaultResponse string
+	url             *url.URL
+}
+
+func newTestBackend(defaultResponse string) (*testBackend, error) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, defaultResponse)
+	}))
+	backendUrl, err := url.Parse(fmt.Sprint("http://", testServer.Listener.Addr().String()))
+	if err != nil {
+		testServer.Close()
+	}
+
+	return &testBackend{
+		server:          testServer,
+		defaultResponse: defaultResponse,
+		url:             backendUrl,
+	}, err
+}
+
+type testFixture struct {
+	cache         *testObjectStorageCache
+	tBlobloServer *httptest.Server
+	tBloblo       *BlobloProxy
+	tBackend      *testBackend
+}
+
+func newTestFixture(t *testing.T) *testFixture {
 	observedZapCore, _ := observer.New(zap.InfoLevel)
 	logger := zap.New(observedZapCore)
 
 	cache := &testObjectStorageCache{}
 
-	backendTestServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, client")
-	}))
-	defer backendTestServer.Close()
-
-	backendUrl, err := url.Parse(fmt.Sprint("http://", backendTestServer.Listener.Addr().String()))
+	backendTestServer, err := newTestBackend("test response")
 	assert.Nil(t, err)
 
-	fallbackReverseProxy := httputil.NewSingleHostReverseProxy(backendUrl)
-	blo := NewBlobloProxy(backendUrl, cache, fallbackReverseProxy, logger)
+	fallbackReverseProxy := httputil.NewSingleHostReverseProxy(backendTestServer.url)
+	blo := NewBlobloProxy(backendTestServer.url, cache, fallbackReverseProxy, logger)
 
-	ts := httptest.NewServer(blo)
-	defer ts.Close()
+	return &testFixture{
+		cache:         cache,
+		tBlobloServer: httptest.NewServer(blo),
+		tBloblo:       blo,
+		tBackend:      backendTestServer,
+	}
+}
+
+func TestFallbackForNotCacheablePath(t *testing.T) {
+	fixture := newTestFixture(t)
+	defer fixture.tBlobloServer.Close()
+	defer fixture.tBackend.server.Close()
 
 	client := http.Client{
 		Timeout: 1 * time.Second,
 	}
-	client.Get(fmt.Sprint(ts.URL, "/some/blob/lo"))
+	resp, err := client.Get(fmt.Sprint(fixture.tBlobloServer.URL, "/some/blob/lo"))
+	assert.Nil(t, err)
+	defer resp.Body.Close()
 
-	assert.False(t, cache.checkedForBlob)
-	assert.False(t, cache.uploadedBlob)
-	assert.False(t, cache.redirectedToPresignedUrl)
+	body, err := ioutil.ReadAll(resp.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, fixture.tBackend.defaultResponse, string(body))
+
+	assert.False(t, fixture.cache.checkedForBlob)
+	assert.False(t, fixture.cache.uploadedBlob)
+	assert.False(t, fixture.cache.redirectedToPresignedUrl)
+}
+
+func TestBlobInitiallyNotInCache(t *testing.T) {
+	cache := &testObjectStorageCache{}
+	testBlobPath := "/v2/blobs/sha256:891b05d87f5e008949d4caf55929c31c3aab0ecbd5ae19e40e8f1421ffd612dd"
+	blobInCache, err := cache.isBlobInCache(testBlobPath)
+
+	assert.False(t, blobInCache)
+	assert.Nil(t, err)
+}
+
+func TestBlobIsUploadedToCacheAndReturnedToClient(t *testing.T) {
+	fixture := newTestFixture(t)
+	defer fixture.tBlobloServer.Close()
+	defer fixture.tBackend.server.Close()
+
+	client := http.Client{
+		Timeout: 1 * time.Second,
+	}
+	cacheablePath := "/v2/blobs/sha256:891b05d87f5e008949d4caf55929c31c3aab0ecbd5ae19e40e8f1421ffd612dd"
+	resp, err := client.Get(fmt.Sprint(fixture.tBlobloServer.URL, cacheablePath))
+	assert.Nil(t, err)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	assert.Nil(t, err)
+
+	assert.Equal(t, fixture.tBackend.defaultResponse, string(body))
+	assert.True(t, fixture.cache.checkedForBlob)
+	assert.True(t, fixture.cache.uploadedBlob)
+	assert.False(t, fixture.cache.redirectedToPresignedUrl)
 }
